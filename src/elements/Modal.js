ec226a88c816daf59d8c724625cec455f788bc41
hyunmyeong <tknfk@naver.com>
2022-07-25 18:16:03 +0900

    모달 컨포넌트 수정

--- src/elements/Modal.js
+++ src/elements/Modal.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 const Modal = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-    const { open, close, alert } = props;
+    const { open, close, alert} = props;
 
     console.log(props)
+
+
 
     return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
@@ -13,13 +15,13 @@ const Modal = (props) => {
             
             {open ? (
             <section>
-                {/* <header>
-                    {header}
-                </header> */}
                     <main>{alert}</main>
                 <footer>
-                    <button className="close" onClick={close}>
-                        닫기
+                    <button className="modal-close-button" 
+                        onClick={()=>{
+                            close()
+                        }}>
+                        확인
                     </button>
                 </footer>
             </section>
