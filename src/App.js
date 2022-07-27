7bd61249dead7263848388b78527839416b692c5
gygy2022 <2022gygy@gmail.com>
2022-07-27 13:29:02 +0900

    App 컴포넌트 Route 재수정 / css 미디어쿼리 css min-width 설정

--- src/App.js
+++ src/App.js
@@ -128,37 +128,36 @@ function App() {
 
 
   return (
-      <div className="App">
-        <Navbar/>
-          <div className="background">
-            <Routes>
-              <Route path="/" element={<Main />}></Route>
-              <Route path="/signup" element={token ? <NotFound /> : <SignUp />}></Route>
-              <Route path="/login" element={token ? <NotFound /> : <Login />}></Route>
-              <Route path="/detail/:id" element={<Detail />}></Route>
-              <Route path="/detail/video/:id" element={<DetailVideo />}></Route>
-              <Route path="/musicfeed" element={<MusicFeed />}></Route>
-              <Route path="/mypage" element={token ? <MyPage /> : <NotFound />}></Route>
-              <Route path="/userpage/:artist" element={<UserPage />}></Route>
-              <Route path="/myedit" element={<MyEdit />}></Route>
-              <Route path="/upload" element={token ? <UploadChoice /> : <NotFound />}></Route>
-              <Route path="/upload/audio" element={token ? <Upload /> : <NotFound />}></Route>
-              <Route path="/upload/video" element={token ? <UploadVideo /> : <NotFound />}></Route>
-              <Route path="/edit/:id" element={token ? <Edit /> : <NotFound />}></Route>
-              <Route path="/live/:artist" element={<Live />}></Route>
-              <Route path="/createlive" element={token ? <CreateLive /> : <NotFound />}></Route>
-              <Route path="/search" element={<SearchResult />}></Route>
-              <Route path="/facechatlist" element={<FaceChatList/>}></Route>
+    <div className="App">
+      <Navbar/>
+        <div className="background">
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/detail/:id" element={<Detail />}></Route>
+            <Route path="/detail/video/:id" element={<DetailVideo />}></Route>
+            <Route path="/musicfeed" element={<MusicFeed />}></Route>
+            <Route path="/mypage" element={<MyPage />}></Route>
+            <Route path="/userpage/:artist" element={<UserPage />}></Route>
+            <Route path="/myedit" element={<MyEdit />}></Route>
+            <Route path="/upload" element={<UploadChoice />}></Route>
+            <Route path="/upload/audio" element={<Upload />}></Route>
+            <Route path="/upload/video" element={<UploadVideo />}></Route>
+            <Route path="/edit/:id" element={<Edit />}></Route>
+            <Route path="/live/:artist" element={<Live />}></Route>
+            <Route path="/createlive" element={ <CreateLive />}></Route>
+            <Route path="/search" element={<SearchResult />}></Route>
+            <Route path="/facechatlist" element={<FaceChatList/>}></Route>
 
-              <Route path="/emailcheck" element={<EmailCheck/>}></Route>
+            <Route path="/emailcheck" element={<EmailCheck/>}></Route>
 
-              <Route path="*" element={<NotFound />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
 
-            </Routes>
-          </div>
-        <Player/>
-      </div>
-
+          </Routes>
+        </div>
+      <Player/>
+    </div>
   );
 }
 
