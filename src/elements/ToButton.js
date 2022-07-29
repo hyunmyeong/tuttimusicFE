e17a94c88fd7c0d0b25e7a96acfa29ee49fa0ea6
hyunmyeong <tknfk@naver.com>
2022-07-29 17:36:14 +0900

    스크롤탑 버튼 컴포넌트

---
+++ src/elements/ToButton.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+
+function ToButton() {
+    const [showButton, setShowButton] = useState(false);
+
+    const scrollToTop = () => {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        })
+
+    }
+    useEffect(() => {
+        const handleShowButton = () => {
+            if (window.scrollY > 500) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        }
+
+        console.log(window.scrollY)
+        window.addEventListener("scroll", handleShowButton)
+        return () => {
+            window.removeEventListener("scroll", handleShowButton)
+        }
+    }, [])
+
+    return showButton && (
+        <div className="scroll-container">
+            <img src='img/scrollTop.png' id="top" onClick={scrollToTop} type="button"/>
+        </div>
+
+    )
+}
+
+export default ToButton
