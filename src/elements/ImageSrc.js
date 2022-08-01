cd3e87998e8a66ed751b24a012b3a2d5ecd881eb
gygy2022 <2022gygy@gmail.com>
2022-08-01 19:48:44 +0900

    배너 이미지 변경, about 페이지 연결

--- src/elements/ImageSrc.js
+++ src/elements/ImageSrc.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ImageUrl = {
   logo_b : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/tutti%EB%A1%9C%EA%B3%A0+%EA%B2%80%EC%9D%80%EC%83%89.png",
   logo_w : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/tutti%EB%A1%9C%EA%B3%A0+%ED%95%98%EC%96%80%EC%83%89.png",
-  banner_tutti_web : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/tutti%EC%84%9C%EB%B9%84%EC%8A%A4%EB%9F%B0%EC%B9%AD+%EA%B2%80%EC%9D%80%EC%83%89+%EB%B0%94%ED%83%95+%EA%B8%B4+%EB%B0%B0%EB%84%88.jpg",
-  banner_tutti_mobile : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/%EB%AA%A8%EB%B0%94%EC%9D%BC+%EB%B0%B0%EB%84%88+%EB%A6%AC%EB%89%B4%EC%96%BC2.jpg",
+  banner_tutti_web : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/%EB%8D%94+%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0-1.jpg",
+  banner_tutti_mobile : "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/%EB%8D%94+%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0-2.jpg",
   banner_event_web: "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/%EC%9D%8C%EC%95%85+%EC%97%85%EB%A1%9C%EB%93%9C%ED%95%98%EA%B3%A0+%EC%BB%A4%ED%94%BC+%EB%B0%9B%EC%95%84%EA%B0%80%EC%84%B8%EC%9A%94.jpg",
   banner_event_mobile: "https://file-bucket-seyeol.s3.ap-northeast-2.amazonaws.com/%EB%AA%A8%EB%B0%94%EC%9D%BC+%EB%B0%B0%EB%84%88+%EB%A6%AC%EB%89%B4%EC%96%BC1.jpg",
 }
